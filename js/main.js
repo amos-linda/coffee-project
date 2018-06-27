f3b85bf347f99e056f70f6f07d61f7155880a90c
Amos Davis <amosdavis@Amoss-MacBook-Pro.local>
2018-06-27 09:24:30 -0500

    starting input functionality

--- js/main.js
+++ js/main.js
@@ -52,26 +52,6 @@ function updateCoffees(e) {
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
 
-// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-// var coffees = [
-//     {id: 1, name: 'Light City', roast: 'light'},
-//     {id: 2, name: 'Half City', roast: 'light'},
-//     {id: 3, name: 'Cinnamon', roast: 'light'},
-//     {id: 4, name: 'City', roast: 'medium'},
-//     {id: 5, name: 'American', roast: 'medium'},
-//     {id: 6, name: 'Breakfast', roast: 'medium'},
-//     {id: 7, name: 'High', roast: 'dark'},
-//     {id: 8, name: 'Continental', roast: 'dark'},
-//     {id: 9, name: 'New Orleans', roast: 'dark'},
-//     {id: 10, name: 'European', roast: 'dark'},
-//     {id: 11, name: 'Espresso', roast: 'dark'},
-//     {id: 12, name: 'Viennese', roast: 'dark'},
-//     {id: 13, name: 'Italian', roast: 'dark'},
-//     {id: 14, name: 'French', roast: 'dark'},
-// ];
-//
-// var ascendingCoffees = coffees.reverse();
-//
 
 // ^^ here are the id nubmers that we have to hide
 
@@ -82,7 +62,11 @@ var roastSelection = document.querySelector('#roast-selection');
 tbody.innerHTML = renderCoffees(coffees);
 // ^^^I've tried to change tbody to div or to the class coffees but it just breaks the table.'
 
+document.getElementById('look4match').addEventListener('input', updateCoffees());
+
+
 submitButton.addEventListener('click', updateCoffees);
+
 
 
 
